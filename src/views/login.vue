c52ea5c13b02e671b000b4b0cfc21dceac21073a
Robaby <1049340826@qq.com>
2022-06-22 21:51:25 +0800

    Add files via upload

---
+++ src/views/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+  <van-button type="primary">主要按钮</van-button>
+<van-button type="success">成功按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+  </div>
+  <div class="box"></div>
+</template>
+
+<script lang="ts" setup>// @ is an alias to /src
+
+
+</script>
+<style lang="scss" scoped>
+  .box{
+    width: 100px;
+    height: 100px;
+    background: red;
+  }
+</style>
